8ec336103b0350f1270cf56e4b58bd891ad2dacb
MATEUS ANDREI DA SILVA FELICIO <0000111590422xsp@aluno.educacao.sp.gov.br>
2023-05-17 14:51:49 -0300

    Funções cript() e descript() modificadas

--- desktop/script.js
+++ desktop/script.js
@@ -3,11 +3,12 @@ let inputEncripted = document.querySelector("#text-cript");
 let textDemo = document.querySelector("#text-demo")
 let image = document.querySelector(".image-high");
 let btnCopy = document.querySelector(".btnCopy");
+let validation = /^[a-z]+$/;
 new ClipboardJS('.btnCopy');
 
 
 function cript() {
-    
+
     /*                  declaração                  */
     let textToCript = inputTextTo.value;
     let emptyText = true;
@@ -25,20 +26,32 @@ function cript() {
     }
 
     if (!emptyText) {
-        /*                 substituição                 */
-        textToCript = textToCript.replace(/a/g, "ai");
-        textToCript = textToCript.replace(/e/g, "enter");
-        textToCript = textToCript.replace(/i/g, "imes");
-        textToCript = textToCript.replace(/o/g, "ober");
-        textToCript = textToCript.replace(/u/g, "ufat");
-        /*----------------------------------------------*/
 
-        /*                style and text                */
-        inputEncripted.style.margin = "32px auto";
-        inputEncripted.innerHTML = '<span class="encripted" style="font-size: 20px">' + textToCript.toLowerCase() + '</span><br><button class="btnCopy" data-clipboard-target=".encripted"><span class="text-button">Copiar texto</span></button';
-        image.style.display = "none";
-        textDemo.style.display = "none";
-        /*----------------------------------------------*/
+        if (validation.test(textToCript)) {
+            /*                 substituição                 */
+            textToCript = textToCript.replace(/a/g, "ai");
+            textToCript = textToCript.replace(/e/g, "enter");
+            textToCript = textToCript.replace(/i/g, "imes");
+            textToCript = textToCript.replace(/o/g, "ober");
+            textToCript = textToCript.replace(/u/g, "ufat");
+            /*----------------------------------------------*/
+
+            /*                style and text                */
+            inputEncripted.style.margin = "32px auto";
+            inputEncripted.innerHTML = '<span class="encripted" style="font-size: 20px">' + textToCript.toLowerCase() + '</span><br><button class="btnCopy" data-clipboard-target=".encripted"><span class="text-button">Copiar texto</span></button';
+            image.style.display = "none";
+            textDemo.style.display = "none";
+            /*----------------------------------------------*/
+        } else {
+
+            alert("O texto contém carácteres especiais!");
+
+        }
+
+    } else {
+
+        alert("Não há texto para criptografar!")
+
     }
 
 }
@@ -61,20 +74,33 @@ function descript() {
     }
 
     if (!emptyText) {
-        /*                 substituição                 */
-        textToCript = textToCript.replace(/ufat/g, "u");
-        textToCript = textToCript.replace(/ober/g, "o");
-        textToCript = textToCript.replace(/imes/g, "i");
-        textToCript = textToCript.replace(/enter/g, "e");
-        textToCript = textToCript.replace(/ai/g, "a");
-        /*----------------------------------------------*/
 
-        /*                style and text                */
-        inputEncripted.style.margin = "32px auto";
-        inputEncripted.innerHTML = '<span class="encripted" style="font-size: 20px">' + textToCript.toLowerCase() + "</span><br><button class='btnCopy' data-clipboard-target='.encripted'><span class='text-button'>Copiar texto</span></button";
-        image.style.display = "none";
-        textDemo.style.display = "none";
-        /*----------------------------------------------*/
+        if (validation.test(textToCript)) {
+
+            /*                 substituição                 */
+            textToCript = textToCript.replace(/ufat/g, "u");
+            textToCript = textToCript.replace(/ober/g, "o");
+            textToCript = textToCript.replace(/imes/g, "i");
+            textToCript = textToCript.replace(/enter/g, "e");
+            textToCript = textToCript.replace(/ai/g, "a");
+            /*----------------------------------------------*/
+
+            /*                style and text                */
+            inputEncripted.style.margin = "32px auto";
+            inputEncripted.innerHTML = '<span class="encripted" style="font-size: 20px">' + textToCript.toLowerCase() + "</span><br><button class='btnCopy' data-clipboard-target='.encripted'><span class='text-button'>Copiar texto</span></button";
+            image.style.display = "none";
+            textDemo.style.display = "none";
+            /*----------------------------------------------*/
+        } else {
+
+            alert("O texto contém carácteres especiais!");
+            
+        }
+        
+    } else {
+        
+        alert("Não há texto para descriptografar!");
+
     }
 
 }
